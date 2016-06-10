ccf1d8f6574e827d90a913e30aec1ddac5b7f56a
thamer90 <thameralmerry@gmail.com>
2016-06-10 15:37:43 +1000

    Added search function and css styles

--- js/app.js
+++ js/app.js
@@ -5,9 +5,21 @@ var myApp  =  angular.module('myApp', []);
 
 myApp.controller('SearchController', function($scope, $http){
 
+    // Fetch JSON data
     $http.get('data.json').then(function(response) {
         $scope.sites = response.data.Sites;
         $scope.categories = response.data.Categories;
     });
+
+    $scope.search = function (row) {
+        // if no entry exists
+        if ($scope.query == '') {
+            return false;
+        }
+        // return entries for names or category inputs
+        else {
+            return !!((row.siteName.toLowerCase().indexOf($scope.query.toLowerCase() || '') !== -1 || row.category.toLowerCase().indexOf($scope.query.toLowerCase() || '') !== -1));
+        }
+    }
 
 });
